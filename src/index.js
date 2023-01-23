8490a4c1f371a2b9fe7cbd17fd439ad297eca6f7
Avineak <ivawudal@gmail.com>
2023-01-23 11:40:22 +0545

    updating admin view

--- src/index.js
+++ src/index.js
@@ -1,30 +1,63 @@
 // dependency added from the php wp
+import { useState } from "@wordpress/element";
+import { Modal } from "@wordpress/components";
+
 wp.blocks.registerBlockType("reduct-plugin/configs", {
   title: "Reduct Video Plugin",
   icon: "smiley",
   category: "common",
   attributes: {
     url: { type: "string" },
-    id: {type: "string"}
   },
+
   // what is seen in admin post editor screen
   edit: function (props) {
-    function updateUrl(e) {
-      props.setAttributes({ url: e.target.value });
+    const [url, setUrl] = useState(props.attributes.url || "");
+    const [isOpen, setOpen] = useState(false);
+    const openModal = () => setOpen(true);
+    const closeModal = () => setOpen(false);
 
-      if(!props.attributes.id) {
-        props.setAttributes({id: Math.random().toString(16).slice(2)})
+    function updateUrl() {
+      if (!url.startsWith("https://app.reduct.video/e/")) {
+        openModal();
+        return;
       }
+      props.setAttributes({ url });
     }
 
     return (
-      <div>
-        <input
-          type="text"
-          placeholder="Put the url here"
-          onChange={updateUrl}
-          value={props.attributes.url}
-        />
+      <div style={{ paddingTop: "20px", paddingBottom: "10px" }}>
+        <h5>Embed Reduct Video</h5>
+        <p>Paste the shared URL</p>
+        <div style={{ display: "flex", width: "100%" }}>
+          <input
+            type="text"
+            placeholder="Enter URL to embed..."
+            onChange={(e) => setUrl(e.target.value)}
+            value={url}
+            style={{ flex: 1, padding: "5px 10px" }}
+          />
+          <button
+            style={{
+              backgroundColor: "rgb(236, 83, 65)",
+              padding: "5px 10px",
+              color: "white",
+              fontSize: "14px",
+              borderRadius: "3px",
+              outline: "none",
+              border: "none",
+              marginLeft: "5px",
+            }}
+            onClick={updateUrl}
+          >
+            Embed
+          </button>
+        </div>
+        {isOpen && (
+          <Modal title="Invalid URL" onRequestClose={closeModal}>
+            <p>Please use a valid url.</p>
+          </Modal>
+        )}
       </div>
     );
   },
