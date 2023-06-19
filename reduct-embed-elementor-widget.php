1cff4963fa026d576b793157136be2f777b656fb
Avineak <ivawudal@gmail.com>
2023-06-19 14:47:05 +0545

    adds image for empty state

--- reduct-embed-elementor-widget.php
+++ reduct-embed-elementor-widget.php
@@ -98,6 +98,14 @@ class Elementor_Reduct_Reel_Embed_Widget extends \Elementor\Widget_Base
 	protected function render()
 	{
 		$settings = $this->get_settings_for_display();
+
+		if ($settings['url'] == '' && $settings['reductDomElement'] == '') {
+			$image = plugin_dir_url(__FILE__) . "src/emptyTranscript.png";
+			echo '<img style="width: 100%" src="' . $image . '" alt="Image">';
+			return;
+		}
+
+
 		$attributes = array("url" => $settings['url'], "domElement" => $settings['reductDomElement'], "uniqueId" => $settings["uniqueId"]);
 		require "template.php";
 	}
